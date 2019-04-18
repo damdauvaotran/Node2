d3a65b7673fbaef7e1b36eb01c9495161ba8ced8
damdauvaotran <ngokhoaisan1999@gmail.com>
2019-04-18 19:40:55 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const url = require("url");
+
+
+const {PORT, HOST} = process.env;
+const httpServer = http.createServer((req, res) => {
+	const pathName = req.url;
+	if ("/hello" === pathName) {
+        const method = req.method;
+        const resMapping = {
+            'GET': 'world',
+            'POST':'world created',
+            'PUT':'world updated',
+            'DELETE':'world deleted'
+        }
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.end(resMapping[method] + "\n");
+	}
+});
+
+
+
+httpServer.listen(PORT||8080);
+console.log("Server started at "+ PORT);
