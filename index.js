4f0a6566781663d9f522a0a7d916cccc9c90d5f2
damdauvaotran <ngokhoaisan1999@gmail.com>
2019-04-25 19:12:11 +0700

    Commit nghiem tuc

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const http = require("http");
 
-
-let port   = process.env.PORT || 3000;
-let host  = process.env.HOST || '127.0.0.1';
+let port = process.env.PORT || 3000;
+let host = process.env.HOST || "127.0.0.1";
 let httpServer = http.createServer((req, res) => {
 	const pathName = req.url;
-	if (pathName =='/hello') {
-        let method = req.method;
-        let resMapping = {
-            'GET': 'world',
-            'POST':'world created',
-            'PUT':'world updated',
-            'DELETE':'world deleted'
-        }
-		res.writeHead(200, {'Content-Type':'text/html'});
-		res.end(resMapping[method]);
-	} 
+	if (pathName == "/hello") {
+		let method = req.method;
+		let response = "";
+		switch (method) {
+			case "GET": {
+                response = "world"
+                break;
+            }
+			case "POST": {
+                response = "world created"
+                break;
+            }
+			case "PUT": {
+                response = "world updated"
+                break;
+            }
+			case "DELETE": {
+                response = "world deleted"
+                break;
+            }
+
+		}
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.end(response);
+	}
 });
 
-httpServer.listen(port , host, (e)=>{
-    console.error(`Server run at ${host}:${port}`);
+httpServer.listen(port, host, e => {
+	console.error(`Server run at ${host}:${port}`);
 });
-
